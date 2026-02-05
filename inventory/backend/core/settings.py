0295c9f2c64bde72cc4e8997e9c0c1559dd8d038
mellowed24 <eyada5@nycstudents.net>
2026-02-05 10:43:30 -0500

    s

--- inventory/backend/core/settings.py
+++ inventory/backend/core/settings.py
@@ -54,8 +54,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),  
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),  
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
