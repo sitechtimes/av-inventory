c4592b1d4a08498e93697b59072fdbfc789f6774
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2026-02-05 10:34:31 -0500

    d

--- inventory/backend/equipment/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Equipment
-
-class EquipmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Equipment
-        fields = ['id', 'name', 'scan_id', 'current_borrower', 'date_checked_out', 'due_date']
