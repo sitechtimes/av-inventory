778f84fbfce4cc0998c17fff01aa56ed9682bef9
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2026-02-12 10:46:13 -0500

    i decided this is all the work for today

--- inventory/backend/equipment/models.py
+++ inventory/backend/equipment/models.py
@@ -2,12 +2,32 @@ from django.db import models
 from student.models import Student
 # Create your models here.
 class Equipment(models.Model):
-    EQUIPMENT_CHOICES = () # van buren
-    name = models.CharField(max_length=20,primary_key=True)#change to shorgter based on how logn code is 
-    equipment_type = models.CharField(max_length=20)#should be shorter
+    EQUIPMENT_CHOICES = [
+        ("BAG", "CAMERA BAG"),
+        ("NXBAT", "CAMERA BATTERY"),
+        ("BOOM", "BOOM POLE"),
+        ("PHBKT", "PHONE BRACKET"),
+        ("TBBKT", "TABLET BRACKET"),
+        ("BCHG", "CAM BATTERY CHARGER"),
+        ("NX100", "CAMERA"),
+        ("HND", "HANDHELD MIC"),
+        ("HDPH", "HEADPHONES"),
+        ("IPAD", "IPAD"),
+        ("LAPCHRG", "LAPTOP CHARGER"),
+        ("LAV", "LAV MIC"),
+        ("LED", "LED LIGHT"),
+        ("TSC-SD", "TASCAM SD CARD"),
+        ("SD", "SD CARD"),
+        ("SHGN", "SHOTGUN MIC"),
+        ("TSCM", "TASCAM"),
+        ("TPD", "TRIPOD INTRO"),
+        ("MANF", "TRIPOD ADV"),
+        ("MONPD", "MONOPOD"),
+        ("XLR","XLR CABLE"),
+    ]
+    name = models.CharField(max_length=15,primary_key=True)
+    equipment_type = models.CharField(max_length=7,choices=EQUIPMENT_CHOICES)
     owner = models.ForeignKey(Student, on_delete=models.SET_NULL)
-
-    
 
     def __str__(self):
         status = "Available" if self.owner =="000000000" else "Checked out" # planning on having van buren be the dummy 000000000 osis number because its not safe to have him be the null option for ovsious reasons.
--- inventory/backend/equipment/views.py
+++ inventory/backend/equipment/views.py
@@ -2,9 +2,16 @@ from rest_framework import viewsets
 from .models import Equipment   
 from .serializers import EquipmentSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 # Create your views here.
 class EquipmentViewSet(viewsets.ModelViewSet):
     queryset = Equipment.objects.all()
     serializer_class = EquipmentSerializer
     permission_classes = [IsAuthenticated]  
-    @action
+    @action(detail=True, methods=['patch'])
+    def update_equipment_owner(self, request, pk=None):
+        equipment = self.get_object()
+        new_owner = request.data.get("owner")
+        equipment.owner = new_owner
+        equipment.save()
+        return Response({"status": "owner updated"})
