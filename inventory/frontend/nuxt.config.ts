176f53267b117144b46e202ce5ce66bf51d8ee05
kongeebowl <kadenpang09@gmail.com>
2026-02-13 11:02:53 -0500

    why is itnopt instakled tailwidn puithis is aaphgeting on m,y nerevces aijopawawjopawejopawe

--- inventory/frontend/nuxt.config.ts
+++ inventory/frontend/nuxt.config.ts
@@ -1,5 +1,10 @@
+import tailwindcss from "@tailwindcss";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
