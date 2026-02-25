64c48b396de9af27f3556e9f8291e6eef0c45636
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2026-02-25 11:52:19 -0500

    smth asbout envs

--- inventory/backend/equipment/tests.py
+++ inventory/backend/equipment/tests.py
@@ -1,6 +1,8 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+
+from inventory.backend.student.models import Student
 from .models import Equipment
 
 
@@ -33,6 +35,18 @@ class EquipmentTests(APITestCase):
         self.assertEqual(Equipment.objects.first().name, "BAG-01")
 
     def test_equipment_list(self):
+        Student.objects.create(
+            osis="121212121",
+            first_name="Samuel",
+            last_name="Samnis",
+            email="kipmuelsamnis@example.com",
+        )
+        Student.objects.create(
+            osis="676767676",
+            first_name="Kipmuel",
+            last_name="Samnis",
+            email="kipmuelsamns@example.com",
+        )
         Equipment.objects.create(
             name="BAG-01",
             equipment_type="CAMERA BAG",
@@ -57,24 +71,29 @@ class EquipmentTests(APITestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
-        self.assertEqual(response.data[0]["first_name"], "Kipnis")
-        self.assertEqual(response.data[1]["first_name"], "Kipmuel")
+        self.assertEqual(response.data[0]["name"], "BAG-01")
+        self.assertEqual(response.data[1]["name"], "BAG-02")
 
-    def test_student_update(self):
+    def test_equipment_update(self):
         student = Student.objects.create(
             osis="123456789",
             first_name="Richard",
             last_name="Kipnis",
             email="richardkipnis@example.com",
         )
+        equipment = Equipment.objects.create(
+            name="BAG-01",
+            equipment_type="CAMERA BAG",
+            owner=student
+        )
 
-        url = reverse("student-detail", kwargs={"pk": student.pk})
-        data = {"first_name": "Samuel"}
+        url = reverse("equipment-detail", kwargs={"pk": equipment.pk})
+        data = {"name": "BAG-02"}
 
         response = self.client.patch(url, data, format="json")
 
-        student.refresh_from_db()
-        print("\n=== STUDENT UPDATE ===")
+        equipment.refresh_from_db()
+        print("\n=== EQUIPMENT UPDATE ===")
         print("UPDATE RESPONSE STATUS:", response.status_code)
         print("UPDATE RESPONSE DATA:", response.data)
         print("=====================\n")
