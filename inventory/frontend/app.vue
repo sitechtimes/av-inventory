3575d8dec8fe5e616a72d8cfdae6a1363944e327
kongeebowl <kadenpang09@gmail.com>
2026-03-03 10:57:57 -0500

    i got tailwind to work YAY

---
+++ inventory/frontend/app.vue
@@ -0,0 +1,1 @@
+<template><p class="text-2xl font-bold">Hello World!</p></template>
--- inventory/frontend/nuxt.config.ts
+++ inventory/frontend/nuxt.config.ts
@@ -1,9 +1,9 @@
-import tailwindcss from "@tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
   },
