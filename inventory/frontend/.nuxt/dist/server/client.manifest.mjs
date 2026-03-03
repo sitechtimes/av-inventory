42a8748223453f3e1391d0b8c0f71e06cf08af21
kongeebowl <kadenpang09@gmail.com>
2026-03-03 10:31:59 -0500

    Delete inventory/frontend/.nuxt directory

--- inventory/frontend/.nuxt/dist/server/client.manifest.mjs
+++
@@ -1,4 +0,0 @@
-
-import { viteNodeFetch } from "file:///C:/Users/kaden.pang23/Documents/GitHub/av-inventory/inventory/frontend/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
-export default () => viteNodeFetch.getManifest()
-    
