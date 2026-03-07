0d3e0755ccc92996f8a3f46a2c719eb6a173aeeb
Benjamin Bernadskiy <bbernadskiy1224@gmail.com>
2026-03-07 00:18:43 -0500

    ed

--- inventory/frontend/layouts/default.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="flex min-h-screen">
-    <SideBar />
-    <main class="ml-44 flex-1 p-6 wrap-break-word">
-      <slot />
-    </main>
-  </div>
-</template>
--- inventory/frontend/pages/students.vue
+++
@@ -1,3 +0,0 @@
-<template><div>Students should probably appear here</div></template>
-
-<script setup lang="ts"></script>
