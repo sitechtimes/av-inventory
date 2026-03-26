1d7f7f3c6e082c61bab69a9e8a5cf8f60a568b65
kongeebowl <kadenpang09@gmail.com>
2026-03-26 11:02:45 -0400

    me is big dummy

--- inventory/frontend/app/pages/add.vue
+++ inventory/frontend/app/pages/add.vue
@@ -2,11 +2,21 @@
   <div class="flex h-full items-center justify-center p-4 md:p-6">
     <div class="panel-shell w-full max-w-xl p-6 text-center">
       <p class="mt-2 text-sm text-base-content/70">
-        <img
+        <!-- <img
           src="https://cdn.discordapp.com/attachments/1289376662814851126/1364998686785933312/IMG_1190.jpg?ex=69c21057&is=69c0bed7&hm=dab3146c0048c4d129bbfb836501a6f750f0c97227131279ba33200fc056e0bc"
           alt=""
-        />
+        /> -->
+        get scanning sir
       </p>
+      <input v-model="inputValue" type="text" autofocus class="hidden" />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const inputValue = ref("");
+
+watch(inputValue, (newValue) => {
+  console.log(newValue);
+});
+</script>
