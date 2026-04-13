35fef61123c36a6aaf280335782b03c7aa7768e6
kongeebowl <kadenpang09@gmail.com>
2026-04-13 11:03:14 -0400

    im gonna shoot benwoneiaeohiawehioawehioaweaweeawawe

--- inventory/frontend/app/pages/add.vue
+++ inventory/frontend/app/pages/add.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="flex h-full items-center justify-center p-4 md:p-6">
+    <button>SELECT STUDENT</button>
+    <button>SCAN IN OR OUT</button>
     <div class="panel-shell w-full max-w-xl p-6 text-center">
       <!-- <img
           src="https://cdn.discordapp.com/attachments/1289376662814851126/1364998686785933312/IMG_1190.jpg?ex=69c21057&is=69c0bed7&hm=dab3146c0048c4d129bbfb836501a6f750f0c97227131279ba33200fc056e0bc"
@@ -24,6 +26,22 @@
 <script setup lang="ts">
 const inputValue = ref("");
 const inputRef = ref<HTMLInputElement>();
+
+const createEquipment = async () => {
+  try {
+    const response = await $fetch("http://localhost:8000/equipment/", {
+      method: "POST",
+      body: {
+        name: "Laptop",
+        equipment_type: "Dell XPS 13",
+        owner: 1200,
+        current_condition: "New",
+      },
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 const handlePaste = (event: any) => {
   event.clipboardData.getData("text/plain");
