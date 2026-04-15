54eb114fe99dec2a5a805fd453465bdddae6db1e
kongeebowl <kadenpang09@gmail.com>
2026-04-15 10:54:22 -0400

    waiting for ben to push........

--- inventory/frontend/app/pages/add.vue
+++ inventory/frontend/app/pages/add.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex h-full items-center justify-center p-4 md:p-6">
-    <button>SELECT STUDENT</button>
-    <button>SCAN IN OR OUT</button>
+    <button @click="fetchUsers">SELECT STUDENT</button>
+    <button @click="status = false">Scan out</button>
+    <button @click="status = true">Scan in</button>
     <div class="panel-shell w-full max-w-xl p-6 text-center">
       <!-- <img
           src="https://cdn.discordapp.com/attachments/1289376662814851126/1364998686785933312/IMG_1190.jpg?ex=69c21057&is=69c0bed7&hm=dab3146c0048c4d129bbfb836501a6f750f0c97227131279ba33200fc056e0bc"
@@ -27,17 +28,30 @@
 const inputValue = ref("");
 const inputRef = ref<HTMLInputElement>();
 
+const status = ref<boolean>();
+
 const createEquipment = async () => {
   try {
     const response = await $fetch("http://localhost:8000/equipment/", {
       method: "POST",
       body: {
-        name: "Laptop",
-        equipment_type: "Dell XPS 13",
-        owner: 1200,
-        current_condition: "New",
+        // name: "Laptop",
+        // equipment_type: "Dell XPS 13",
+        // owner: 1200,
+        // current_condition: "New",
       },
     });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const fetchUsers = async () => {
+  try {
+    const response = await $fetch("http://localhost:8000/student/", {
+      method: "GET",
+    });
+    return response;
   } catch (e) {
     console.error(e);
   }
