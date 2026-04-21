5ec718d9326cde08adbd3325413f7234a9d9998e
kongeebowl <kadenpang09@gmail.com>
2026-04-21 10:57:44 -0400

    no more ben

--- inventory/backend/core/urls.py
+++ inventory/backend/core/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import LoginView, SessionView, LogoutView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth/login/", LoginView.as_view(), name="auth_login"),
-    path("api/auth/session/", SessionView.as_view(), name="auth_session"),
-    path("api/auth/logout/", LogoutView.as_view(), name="auth_logout"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("student/", include("student.urls")),
     path("equipment/", include("equipment.urls")),
 ]
