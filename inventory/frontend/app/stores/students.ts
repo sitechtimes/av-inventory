0f0f31c36910a8c4c9739164436491e81d6606dd
kongeebowl <kadenpang09@gmail.com>
2026-04-29 11:01:28 -0400

    j

--- inventory/frontend/app/stores/students.ts
+++ inventory/frontend/app/stores/students.ts
@@ -4,7 +4,8 @@ export const useStudentStore = defineStore("students", () => {
   const students = ref<Student[]>([]);
 
   async function fetchStudents() {
-    const { data, error } = await tryRequestEndpoint("student/students/");
+    const { data, error } =
+      await tryRequestEndpoint<Student[]>("student/students/");
     if (error) return console.error("Error fetching students:", error);
     students.value = data;
   }
